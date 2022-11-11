c13776354b359096737e7568f7fa29513a730fb1
ksears12 <ksears500@gmail.com>
2022-11-11 16:29:17 -0500

    radiansconversion

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -61,7 +61,7 @@ def calc_road_speed(gear: int, rpm: int, car, transmission_efficiency: int = 0.9
     tire_radius = car.attrs["tire_radius"]
     gear_ratio = car.attrs["gear_ratio"][gear]
 
-    return ((rpm / (final_drive * gear_ratio * transmission_efficiency) * 6) * 3.14) / 180 * tire_radius
+    return math.radians(((rpm / (final_drive * gear_ratio * transmission_efficiency) * 6)))* tire_radius
 
 
 def calc_torque_at_wheels(gear: int, torque: float, car, transmission_efficiency: int = 0.9):
