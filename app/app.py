b83df873320d96075e5d6a84c61fbfd63886b150
benjaminrome <benjamin.rome10@gmail.com>
2023-01-14 19:41:49 -0500

    basics of bootstrap

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template('basic.html')
