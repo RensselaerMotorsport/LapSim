3c37a9f89bf7e1bcad1cf7ad6154946bdc0f4a37
ColeAugusta <colepaugusta@gmail.com>
2023-01-24 17:37:25 -0500

    Flask_development

--- app/app.py
+++ app/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('basic.html')
+
+@app.route("/InputForm")
+def input_form():
+    return render_template('inputform.html')
