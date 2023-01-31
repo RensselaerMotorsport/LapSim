33ebee4ecb63d83e15a6c8d79edecaa6482640f2
evanf26 <115671591+evanf26@users.noreply.github.com>
2023-01-31 17:10:06 -0500

    rm26 form "fixed" and retrieving data

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,9 @@ import os #get current working directory for json file
 #rm26_data = json.loads(open('C:\\Users\\fornem\\Dropbox\\lapsim\\lapsim_final\\rm26.json').read())
 
 #each perimeter from the json file if the user wants to change them
-#no data is required
+#no data is required, a blank field should use the json data
+#the data returned from these forms will be plugged into the helper functions (need to figure out how to incorporate those)
+#helper functions and straight line sim program are found in src branch of repo
 class straightLineForm25(FlaskForm):
 
     directory = os.getcwd()
@@ -36,4 +38,34 @@ class straightLineForm25(FlaskForm):
     diff_sprocket_teeth = StringField('Diff Sprocket Teeth ('+str(rm25_data['diff_sprocket_teeth'])+')')
     tire_radius = StringField('Tire Radius ('+str(rm25_data['tire_radius'])+')')
     gear_ratios = StringField('Gear Ratios ('+str(rm25_data['gear_ratios'])+')')
+    submit = SubmitField('Calculate')
+
+class straightLineForm26(FlaskForm):
+
+    directory = os.getcwd()
+    rm26_data = json.loads(open(directory+"\\app\\rm26.json").read())
+
+    #each field for data to be entered
+    mass_car = StringField('Car Mass ('+str(rm26_data['mass_car'])+')')
+    mass_driver = StringField('Driver Mass ('+str(rm26_data['mass_driver'])+')')
+    proportion_front = StringField('Proportion Front ('+str(rm26_data['proportion_front'])+')')
+    front_track_width = StringField('Front Track Width ('+str(rm26_data['front_track_width'])+')')
+    rear_track_width = StringField('Rear Track Width ('+str(rm26_data['rear_track_width'])+')')
+    wheelbase = StringField('Wheelbase ('+str(rm26_data['wheelbase'])+')')
+    CG_height = StringField('Center of Gravity Height ('+str(rm26_data['CG_height'])+')')
+    yaw_inertia = StringField('Yaw Inertia ('+str(rm26_data['yaw_inertia'])+')')
+    CoF = StringField('Coefficient of Friction ('+str(rm26_data['CoF'])+')')
+    load_sensitivity = StringField('Load Sensitivity ('+str(rm26_data['load_sensitivity'])+')')
+    Cd = StringField('Coefficient of Drag ('+str(rm26_data['Cd'])+')')
+    Cl = StringField('Coefficient of Lift ('+str(rm26_data['Cl'])+')')
+    A = StringField('Frontal Area ('+str(rm26_data['A'])+')')
+    rho = StringField('Rho ('+str(rm26_data['rho'])+')')
+    front_downforce = StringField('Front Downforce ('+str(rm26_data['front_downforce'])+')')
+    cp_height = StringField('Center of Pressure Height ('+str(rm26_data['cp_height'])+')')
+    brake_bias = StringField('Brake Bias ('+str(rm26_data['brake_bias'])+')')
+    primary_drive = StringField('Primary Drive ('+str(rm26_data['primary_drive'])+')')
+    engine_sprocket_teeth = StringField('Engine Sprocket Teeth ('+str(rm26_data['engine_sprocket_teeth'])+')')
+    diff_sprocket_teeth = StringField('Diff Sprocket Teeth ('+str(rm26_data['diff_sprocket_teeth'])+')')
+    tire_radius = StringField('Tire Radius ('+str(rm26_data['tire_radius'])+')')
+    gear_ratios = StringField('Gear Ratios ('+str(rm26_data['gear_ratios'])+')')
     submit = SubmitField('Calculate')
--- app/main.py
+++ app/main.py
@@ -13,13 +13,37 @@ def homepage():
 #straight line simulation page
 @app.route('/rm25_straight_line_sim', methods=['GET', 'POST'])
 def rm25_straight_line_sim():
+
     form = straightLineForm25('/rm25_straight_line_sim')
-    mass_car = None
 
     # Submit Form
     if form.is_submitted():
-         mass_car = form.mass_car.data # Taking in data maybe
-         return render_template('output.html') # Redirect to a different page if needed
+
+        #retreiving data (need to do tests on what empty fields return)
+        mass_car = form.mass_car.data # Taking in data
+        mass_driver = form.mass_driver.data
+        proportion_front = form.proportion_front.data
+        front_track_width = form.front_track_width.data
+        rear_track_width = form.rear_track_width.data
+        wheelbase = form.wheelbase.data
+        CG_height = form.CG_height.data
+        yaw_inertia = form.yaw_inertia.data
+        CoF = form.CoF.data
+        load_sensitivity = form.load_sensitivity.data
+        Cd = form.Cd.data
+        Cl = form.Cl.data
+        A = form.A.data
+        rho = form.rho.data
+        front_downforce = form.front_downforce.data
+        cp_height = form.cp_height.data
+        brake_bias = form.brake_bias.data
+        primary_drive = form.primary_drive.data
+        engine_sprocket_teeth = form.engine_sprocket_teeth.data
+        diff_sprocket_teeth = form.diff_sprocket_teeth.data
+        tire_radius = form.tire_radius.data
+        gear_ratios = form.gear_ratios.data
+
+        return render_template('output.html') # Redirect to a different page if needed
 
     return render_template('rm25_straight_line_sim.html', title="RM25 Staight Line Sim", form=form)
 
