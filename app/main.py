c1577d28803b684cd07b048298e69144fe436d81
Tre'Vaughn Barboza <trerese3@gmail.com>
2023-02-03 17:07:49 -0500

    JSON creation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for #basic flask modules
 from forms import straightLineForm25#, straightLineForm26 #classes from forms.py
+import json
 
 app = Flask(__name__)
 
@@ -42,6 +43,36 @@ def rm25_straight_line_sim():
         diff_sprocket_teeth = request.form['diff_sprocket_teeth']
         tire_radius = request.form['tire_radius']
         gear_ratios = request.form['gear_ratios']
+
+        dict = {
+            'mass_car': mass_car, 
+            'mass_driver': mass_driver,
+            'proportion_front': proportion_front,
+            'front_track_width': front_track_width,
+            'rear_track_width': rear_track_width,
+            'wheelbase': wheelbase,
+            'CG_height': CG_height,
+            'yaw_inertia': yaw_inertia,
+            'CoF': CoF,
+            'load_sensitivity': load_sensitivity,
+            'Cd': Cd,
+            'Cl': Cl,
+            'A': A,
+            'rho': rho,
+            'front_downforce': front_downforce,
+            'cp_height': cp_height,
+            'brake_bias': brake_bias,
+            'primary_drive': primary_drive,
+            'engine_sprocket_teeth': engine_sprocket_teeth,
+            'diff_sprocket_teeth': diff_sprocket_teeth,
+            'tire_radius': tire_radius,
+            'gear_ratios': gear_ratios
+        }
+
+        json_obj = json.dumps(dict, indent=4)
+
+        with open('data.json', 'w') as outfile:
+            outfile.write(json_obj)
         
         return render_template('output.html') # Redirect to a different page if needed
 
