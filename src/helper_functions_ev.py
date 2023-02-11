37a382c8728f7c297edb358412926a5e5738a47f
Levi Hlavac <112892358+LeviHl@users.noreply.github.com>
2023-02-10 22:49:39 -0500

    added equation to helper_functions_ev

--- src/helper_functions_ev.py
+++ src/helper_functions_ev.py
@@ -2,6 +2,24 @@
 
 import math
 
+def calc_vmax(r, car):
+    """
+    Calculates the maximum velocity for a corner. 
+    """
+    mew = car.attrs["CoF"]
+    m = car.attrs["mass_car"] + car.attrs["mass_driver"]
+    Cd = car.attrs["Cd"]
+    rho = car.attrs["rho"]
+    A = car.attrs["A"]
+    Cl = car.attrs["Cl"]
+
+    g = 9.8 
+
+
+    num = 2*r*((-g*m*rho*Cl*A*r*mew**2) + (g*m*mew*(math.sqrt((Cd**2)*(rho**2)*(A**2)*(r**2)+(4*m**2)))))
+    dem = (r**2)*((rho**2)*(Cl**2)*(A**2)*(mew**2) - (Cd**2)*(rho**2)*(A**2)) - 4*(m**2)
+    return math.sqrt(num/dem)
+
 
 def calculate_velocity_new(engine_force, drag_force, car, step=1, initial_velocity=0.001):
     """
@@ -20,41 +38,6 @@ def calculate_velocity_new(engine_force, drag_force, car, step=1, initial_veloci
 
     return math.sqrt((initial_velocity**2) + 2 * step * ((engine_force - drag_force) / (car_mass + driver_mass)))
 
-
-def calculate_drag_force(car, initial_velocity=0.001):
-    """
-    A function for calculating the drag force of a provided car at a provided velocity.
-    
-    Given: car, the car object we are testing
-    initial_velocity, the velocity at the begining of the time step we are running, default is .001
-    
-    Returns: the drag froce of the car at the provided velocity
-    """
-    coeff_drag = car.attrs["Cd"]
-    air_density = car.attrs["rho"]
-    frontal_area = car.attrs["A"]
-
-    return coeff_drag * 0.5 * air_density * (initial_velocity ** 2) * frontal_area
-
-
-def calculate_friction_force(car, initial_velocity=0):
-    """
-    A function for calculating the friction force of a car at an initial velocity.
-    
-    Given: car, the car object we are testing
-    initial_velocity, the initial velocoty of the car at a time step, default is 0
-    
-    Returns: the friction force at the provided conditions.
-    """
-    coeff_friction = car.attrs["CoF"]
-    car_mass = car.attrs["mass_car"]
-    driver_mass = car.attrs["mass_driver"]
-    coeff_lift = car.attrs["Cl"]
-    air_density = car.attrs["rho"]
-    frontal_area = car.attrs["A"]
-
-    return coeff_friction * ((car_mass + driver_mass) * 9.81) + (coeff_lift * 0.5 * air_density * (initial_velocity**2) * frontal_area)
-
     
 def get_drag_force(velocity: float, car):
     """Calculates drag force given a velocity"""
@@ -63,21 +46,6 @@ def get_drag_force(velocity: float, car):
     frontal_area = car.attrs["A"]
 
     return velocity**2 * coeff_drag * .5 * rho * frontal_area
-
-
-def calc_corner_radius(coord1, coord2, coord3):
-    """
-    Calculates radius of corner at any given sector
-    Coord1:Starting point of previous sector
-    Coord2:Starting point of current sector
-    Coord3:Starting point of next sector
-    All inputs are in 1D two element numpy arrays
-    """
-    a = math.sqrt((coord3[1]-coord1[1])**2 + (coord3[2]-coord1[2])**2)
-    b = math.sqrt((coord3[1]-coord2[1])**2 + (coord3[2]-coord2[2])**2)
-    c = math.sqrt((coord2[1]-coord1[1])**2 + (coord2[2]-coord1[2])**2)
-    return math.acos((b**2+c**2-a**2)/2*b*c)
-
 
 def calc_lat_accel(car, v, icr):
     """
@@ -111,17 +79,4 @@ def calc_t(v1, v2, d_step):
 #not sure how we should distinguish the difference between long and lat velocities
 #good thing is we know all long accel is caused by engine force and any lat accel is caused by curavture
 
-def calc_long_accel(v1, v2, t):
-    """
-    Calculates longitudinal acceleration
 
-    Inputs:
-    v1 - velocity at the begining of segment n
-    v2 - velocity at the end of segment n
-    t - the time it takes to complete segment n
-
-    Output:
-    Longitudinal acceleration
-    """
-
-    return (v1+v2)/t
