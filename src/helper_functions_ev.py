98891274c2affed2ce3e1d4195f6ca099506db2e
Levi Hlavac <112892358+LeviHl@users.noreply.github.com>
2023-02-14 18:59:04 -0500

    added skidpad scipt

--- src/helper_functions_ev.py
+++ src/helper_functions_ev.py
@@ -4,7 +4,7 @@ import math
 
 def calc_vmax(r, car):
     """
-    Calculates the maximum velocity for a corner. 
+    Calculates the maximum velocity for a corner given it's radius. 
     """
     mew = car.attrs["CoF"]
     m = car.attrs["mass_car"] + car.attrs["mass_driver"]
@@ -16,9 +16,16 @@ def calc_vmax(r, car):
     g = 9.8 
 
 
-    num = 2*r*((-g*m*rho*Cl*A*r*mew**2) + (g*m*mew*(math.sqrt((Cd**2)*(rho**2)*(A**2)*(r**2)+(4*m**2)))))
-    dem = (r**2)*((rho**2)*(Cl**2)*(A**2)*(mew**2) - (Cd**2)*(rho**2)*(A**2)) - 4*(m**2)
-    return math.sqrt(num/dem)
+    num = (-m*g*Cl*A*(mew**2)*rho*r) - (g*m*mew*(math.sqrt((Cd**2)*(rho**2)*(A**2)*(r**2)+(4*(m**2)))))
+    dem = (2*r)*(.25*(rho**2)*(Cl**2)*(A**2)*(mew**2) - .25*(Cd**2)*(rho**2)*(A**2)-((m**2)/(r**2)))
+
+    return (math.sqrt(abs(num/dem)))
+    #There are 4 possible solutions to this problem. Two have been selected (through abs) as possible real solutions as the others are negative. 
+    #The other two solutions differ by the minus or plus sign in the numerator between both large terms.
+    #The negative sign was chosen as the only real solution because this yields real results when changing Cl on skidpad. 
+    #It is possible that this solution does not always yield a real result and the other solutions are real but this will be something we come back to. 
+    
+    #This model includes 
 
 
 def calculate_velocity_new(engine_force, drag_force, car, step=1, initial_velocity=0.001):
