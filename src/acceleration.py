34599f9cc4c7edcb520d7c752245b255c2a6314a
erichbahm <97556713+erichbahm@users.noreply.github.com>
2023-02-14 17:13:04 -0500

    Testing run_accel with print statements.

--- src/acceleration.py
+++ src/acceleration.py
@@ -1,8 +1,13 @@
 """Acceleration event calculation."""
 
-import math
-from helper_functions_ev.py import calc_vmax
-from helper_functions_ev import straightlinesegment
+#import math
+#from helper_functions_ev.py import calc_vmax
+from classes.car_simple import Car
+from helper_functions_ev import straightlinesegmenttime
+
+car = Car("data/rm26.json")
 
 def run_accel(car):
-    return straightlinesegment(car, 75, vinitial= 0.001)
+    return straightlinesegmenttime(car, 75, vinitial= 0.001)
+
+print(run_accel(car))
