c8b6ff1cfd3a18fc099c0c9faf8486e2a1b68b3d
erichbahm <97556713+erichbahm@users.noreply.github.com>
2023-02-14 17:26:11 -0500

    run_accel now returns only time elapsed.

--- src/acceleration.py
+++ src/acceleration.py
@@ -8,6 +8,5 @@ from helper_functions_ev import straightlinesegmenttime
 car = Car("data/rm26.json")
 
 def run_accel(car):
-    return straightlinesegmenttime(car, 75, vinitial= 0.001)
+    return straightlinesegmenttime(car, 75, vinitial= 0.001)[0]
 
-print(run_accel(car))
