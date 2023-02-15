9b46d67e21398b19d9aed35f7d637dc9b261910e
erichbahm <97556713+erichbahm@users.noreply.github.com>
2023-02-15 00:25:06 -0500

    Added test_accel to output a graph of gear ratios and their times at accel.

--- src/acceleration.py
+++ src/acceleration.py
@@ -1,12 +1,29 @@
 """Acceleration event calculation."""
 
-#import math
-#from helper_functions_ev.py import calc_vmax
 from classes.car_simple import Car
-from helper_functions_ev import straightlinesegmenttime
+from helper_functions_ev import line_segment_time
 
 car = Car("data/rm26.json")
 
 def run_accel(car):
-    return straightlinesegmenttime(car, 75, vinitial= 0.001)[0]
+    result = line_segment_time(car, 75, vinitial= 0.001)
+    #print(result[1])
+    return result[0]
 
+def test_accel(car, LG, UG, n=100):
+    car.attrs["gear_ratios"] = LG
+    step = (UG - LG) / n
+    x = []
+    y = []
+    for i in range(n):
+        x.append(car.attrs["gear_ratios"])
+        y.append(run_accel(car))
+        car.attrs["gear_ratios"] += step
+    import matplotlib.pyplot as plt
+    plt.title("Acceleration event times", fontsize=18, y=1.04)
+    plt.xlabel("Gear Ratio", fontsize=12)
+    plt.ylabel("Time elapsed (s)", fontsize=12)
+    plt.plot(x, y, '-g')
+    plt.show()
+
+test_accel(car, 5.8, 6.2, n=100)
