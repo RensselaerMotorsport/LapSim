0045d6143cb6859f119d407f26400ff85b68e29f
ksears12 <ksears500@gmail.com>
2023-02-17 17:16:13 -0500

    Acclerationcdocumentation

--- src/acceleration.py
+++ src/acceleration.py
@@ -1,4 +1,4 @@
-"""Acceleration event calculation."""
+"""A module to simulate the acceleration event."""
 
 from classes.car_simple import Car
 from helper_functions_ev import line_segment_time
@@ -6,26 +6,40 @@ from helper_functions_ev import line_segment_time
 car = Car("data/rm26.json")
 
 def run_accel(car, tmin=4.113, returnPoints=False):
-    """ Calculates time to travel 75 m in a straight line. Can return points instead of time elapsed.
+    """ Calculates the amount of time it takes to run accel. Can return an approximate point value for the event instead of time elapsed.
+    Assumptions: Acceleration is 75 m long
+    Starting velocity of the car is .001 m/s
 
-    car: car
-    tmin: Best achieved accel time at comp
-    returnPoints: Whether or not to return results as point values or time values
-    return:
+    Inputs:
+        car: the car object that we are working with
+        tmin: best acheieved acceleration time at competition, base value is 4.113 seconds
+        returnPoints: if set to True it will return how many points you would earn from accel instead of the time, default is False
+    Returns:
+        t, the amount of time it takes to complete accel
+        or if returnPoints=True, it will return an approximate point value for the event
     """
-    t = line_segment_time(car, 75, vinitial= 0.001)
+    t = line_segment_time(car, 75, vinitial= 0.001) #Call the helper function that runs a straight line segment
     if returnPoints:
-        if t[0] >= 1.5 * tmin: return 4.5
-        elif t[0] <= tmin: return 100
-        else: return 4.5 + 95.5 * 2 * (1.5 * tmin/t[0] - 1)
+        if t[0] >= 1.5 * tmin: return 4.5 #The time specified is below 150% of the fastest achieved accleration time, so the point value returned is 4.5 points
+        elif t[0] <= tmin: return 100 #The time specified is faster or equal to the fasted achieved accleration time, maxiumum points have been achieved
+        else: return 4.5 + 95.5 * 2 * (1.5 * tmin/t[0] - 1) #The time specfied is slower than the fastest time, but faster than 150% of it, so the stupid equation is used 
     else: return t[0]
 
 def test_accel(car, LG, UG, n=100, returnPoints=False):
-    car.attrs["gear_ratios"] = LG
-    step = (UG - LG) / n
+    """A function for testing the response of our run_accel function.
+    
+    Inputs: car, the car object we are working with
+    LG, the car's lower bound gear ratio
+    UG, the car's upper bound gear ratio
+    n, this equals the number of gear ratios that we want to test, default is 100
+    returnPoints, this determines if the function returns point values or time values for accleration, default is False which returns time values
+    
+    Returns: A plot of gear ratios tried to point values achieved if returnPoints=False, or to time is returnPoints=True"""
+    car.attrs["gear_ratios"] = LG #This is the lowest gear ratio we are going to try
+    step = (UG - LG) / n #This calculates the change in the different gear ratios we are going to try
     x = []
     y = []
-    for i in range(n):
+    for i in range(n): #iterate over all gear ratios
         x.append(car.attrs["gear_ratios"])
         y.append(run_accel(car, returnPoints=returnPoints))
         car.attrs["gear_ratios"] += step
