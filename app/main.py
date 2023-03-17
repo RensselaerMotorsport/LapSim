a22ff6ad66e77ad1de564b2ab4b14a140ef28ce2
Tre'Vaughn Barboza <trerese3@gmail.com>
2023-03-17 19:42:59 -0400

    Seep Function Foundation
    The Sweep function now works for simple things. More testing and development needs to be done in order to make it work consistently.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response, flash, redirect #basic flask modules
+from flask import Flask, render_template, request, make_response, flash, redirect, url_for #basic flask modules
 from forms import straightLineForm25, rm_26_acceleration_form #, straightLineForm26 #classes from forms.py
 import itertools #for looping through all possible combinations of sweep values
 import numpy #for generating range of sweep values
@@ -24,18 +24,18 @@ def homepage():
 
 def fill_blank_with_default(i, car):
         if request.form[i] != '':
-                        #because gear_ratios are stored as a list in the json file, we need a special case to split it by commas
-                        # if i == 'gear_ratios':
-                        #     car[i] = request.form[i].replace(' ','').split(',')
-                        #     #removing any blank values (in case multiple commas in a row: 1, 2, 3,,,, 4)
-                        #     #goes backwards through list so we don't get insex out of range
-                        #     for j in range(len(car[i])-1,-1,-1):
-                        #         if car[i][j] == '':
-                        #             car[i].remove('')
-                        #         else:
-                        #             car[i][j] = float(car[i][j])
-                        # #otherwise, we can just convert to float
-                        # else:
+            # because gear_ratios are stored as a list in the json file, we need a special case to split it by commas
+            if i == 'gear_ratios':
+                car[i] = request.form[i].replace(' ','').split(',')
+                #removing any blank values (in case multiple commas in a row: 1, 2, 3,,,, 4)
+                #goes backwards through list so we don't get insex out of range
+                for j in range(len(car[i])-1,-1,-1):
+                    if car[i][j] == '':
+                        car[i].remove('')
+                    else:
+                        car[i][j] = float(car[i][j])
+            else:
+                #otherwise, we can just convert to float
                 car[i] = float(request.form[i])
 
 #straight line simulation page
@@ -93,6 +93,7 @@ def rm25_straight_line_sim():
                 sweep_values.append(list(numpy.arange(float(values[key][0]), float(values[key][2])+1, float(values[key][1]))))
             sweep_combos = list(itertools.product(*sweep_values))
 
+            resp = make_response(redirect('/output'))
             #loop through all possible combinations of sweep values
             for combo in sweep_combos:
                 #ERROR: breaks here
@@ -102,7 +103,6 @@ def rm25_straight_line_sim():
                     else:
                         fill_blank_with_default(filtered_keys[i], car)
                 json_obj = json.dumps(car, indent=2)
-                resp = make_response(output())
 
                 # FOR DEBUGGING
                 # with open('output'+str(combo)+'.json', 'w') as f:
@@ -172,27 +172,30 @@ def rm26_acceleration():
             for key in values:
                 sweep_values.append(list(numpy.arange(float(values[key][0]), float(values[key][2])+1, float(values[key][1]))))
             sweep_combos = list(itertools.product(*sweep_values))
-
+            
+            sweep_combos_str = json.dumps(sweep_combos)
+            resp = make_response(redirect(url_for('output', sweep_toggled=sweep_toggled, sweep_combos_str=sweep_combos_str)))
             #loop through all possible combinations of sweep values
             for combo in sweep_combos:
-                #ERROR: breaks here
                 for i in range(min(len(filtered_keys), len(combo))):
                     if filtered_keys[i] in values:
                         car[filtered_keys[i]] = combo[i]
                     else:
-                        fill_blank_with_default(filtered_keys[i], car)
+                        if request.form[i] != '':
+                            car[i] = float(request.form[i])
                 json_obj = json.dumps(car, indent=2)
-                resp = make_response(output())
 
                 # FOR DEBUGGING
-                # with open('output'+str(combo)+'.json', 'w') as f:
-                #     f.write(json_obj)
+                with open('output'+str(combo)+'.json', 'w') as f:
+                    f.write(json_obj)
 
                 resp.set_cookie('data' + str(combo), json_obj)
 
         else:
             for i in filtered_keys:
-                fill_blank_with_default(i, car)
+                if request.form[i] != '':
+                    car[i] = float(request.form[i])
+
             json_obj = json.dumps(car, indent=2)
             resp = make_response(redirect('/output'))
             resp.set_cookie('data', json_obj)
@@ -204,9 +207,23 @@ def rm26_acceleration():
 
 @app.route('/output', methods=['GET', 'POST'])
 def output():
+    # Retrive args
+    sweep_toggled = request.args.get('sweep_toggled')
+    
+    if (sweep_toggled):
+        # Retrive args
+        sweep_combos = tuple(tuple(x) for x in json.loads(request.args.get('sweep_combos_str'))) 
+        time = []
+        for combo in sweep_combos:
+            data = request.cookies.get('data' + str(combo))
+            if data:
+                car = Car(data)
+                time.append(run_accel(car))
+        return render_template('output.html', time=time)
     data = request.cookies.get('data')
     car = Car(data)
-    time = run_accel(car)
+    time = []
+    time.append(run_accel(car))
     return render_template('output.html', time=time)
 
 if __name__ == "__main__":
