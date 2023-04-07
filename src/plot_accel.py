23aeef538f81567ec1e68109d0ec8716111a05d5
erichbahm <97556713+erichbahm@users.noreply.github.com>
2023-04-07 16:28:14 -0400

    Deleted unused script

--- src/plot_accel.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import pyplot as plt
-from helper_functions_ev import forward_int
-from classes.car_simple import Car
-car = Car("data/rm26.json")
-
-plt.title("v")
-plt.plot(forward_int(car, 0,20)[0])
-plt.show()
