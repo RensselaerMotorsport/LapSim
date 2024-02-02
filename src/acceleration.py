ac321b05eaaa50916ae10ba4229b91a542252bdf
erichbahm <97556713+erichbahm@users.noreply.github.com>
2024-02-02 16:35:52 -0500

    Fixed acceleration calculation using new helper functions

--- src/acceleration.py
+++ src/acceleration.py
@@ -1,31 +1,31 @@
 """A module to simulate the acceleration event."""
 
 from classes.car_simple import Car
-from helper_functions_ev import line_segment_time
+from helper_functions_ev import straight_line_segment
 
-# car = Car("data/rm26.json")
+car = Car("data/rm27.json")
 
-def run_accel(car, GR=0, tmin=4.113, returnPoints=False, peak=False, mu=0):
+def run_accel(car, GR=0, tmin=3.645, returnPoints=False, peak=False, mu=0):
     """ Calculates the amount of time it takes to run accel. Can return an approximate point value for the event instead of time elapsed.
     Assumptions: Acceleration is 75 m long
     Starting velocity of the car is .001 m/s
 
     Inputs:
         car: the car object that we are working with
-        tmin: best acheieved acceleration time at competition, base value is 4.113 seconds
+        tmin: best achieved acceleration time at competition, base value is 3.645 seconds
         returnPoints: if set to True it will return how many points you would earn from accel instead of the time, default is False
     Returns:
         t, the amount of time it takes to complete accel
         or if returnPoints=True, it will return an approximate point value for the event
     """
     if GR == 0: GR = car.attrs["final_drive"]
-
-    t = line_segment_time(car, 75, GR=GR, vinitial= 0.001, peak=peak, mu=mu) #Call the helper function that runs a straight line segment
+    if mu == 0: mu = car.attrs["CoF"]
+    t = straight_line_segment(car,0.00001, 100, 75, GR=GR, peak=peak, mu=mu, returnV=False) #Call the helper function that runs a straight line segment
     if returnPoints:
-        if t[0] >= 1.5 * tmin: return 4.5 #The time specified is below 150% of the fastest achieved accleration time, so the point value returned is 4.5 points
-        elif t[0] <= tmin: return 100 #The time specified is faster or equal to the fasted achieved accleration time, maxiumum points have been achieved
-        else: return 4.5 + 95.5 * 2 * (1.5 * tmin/t[0] - 1) #The time specfied is slower than the fastest time, but faster than 150% of it, so the stupid equation is used 
-    else: return t[0]
+        if t >= 1.5 * tmin: return 4.5 #The time specified is below 150% of the fastest achieved accleration time, so the point value returned is 4.5 points
+        elif t <= tmin: return 100 #The time specified is faster or equal to the fasted achieved accleration time, maxiumum points have been achieved
+        else: return 4.5 + 95.5 * 2 * (1.5 * tmin/t - 1) #The time specfied is slower than the fastest time, but faster than 150% of it, so the stupid equation is used
+    else: return t
 
 
 def test_accel(car, LG, UG, n=100, returnPoints=False, peak=False):
@@ -43,16 +43,18 @@ def test_accel(car, LG, UG, n=100, returnPoints=False, peak=False):
     step = (UG - LG) / n  # This calculates the change in the different gear ratios we are going to try
     x = []
     y = []
+    GR = LG
     for i in range(n):  # iterate over all gear ratios
-        x.append(car.attrs["gear_ratios"])
-        y.append(run_accel(car, returnPoints=returnPoints, peak=peak))
-        car.attrs["gear_ratios"] += step
+        x.append(GR)
+        y.append(run_accel(car, GR=GR, returnPoints=returnPoints, peak=peak))
+        GR += step
     import matplotlib.pyplot as plt  # Create the plot of gear ratio versus time
-    plt.title("Acceleration event times", fontsize=18, y=1.04)
+    plt.xlim(LG,UG)
+    plt.grid()
+    if returnPoints: plt.title("Acceleration event points", fontsize=18, y=1.04)
+    else: plt.title("Acceleration event times", fontsize=18, y=1.04)
     plt.xlabel("Gear Ratio", fontsize=12)
-    plt.ylabel("Time elapsed (s)", fontsize=12)
+    if returnPoints: plt.ylabel("Points scored", fontsize=12)
+    else: plt.ylabel("Time elapsed (s)", fontsize=12)
     plt.plot(x, y, '-g')
     plt.show()
-
-
-#test_accel(car, 0.1, 10, returnPoints=True)
