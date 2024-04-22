71013805a6c5803bf39fde353ca4028e57f2e000
erichbahm <97556713+erichbahm@users.noreply.github.com>
2024-04-21 21:55:38 -0400

    Fixed calculation errors, and implemented reflected inertia calculation

--- src/drivetrain_module.py
+++ src/drivetrain_module.py
@@ -1,43 +1,42 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-
 class Drivetrain:
-    def __init__(self):
+    def __init__(self, Tmotor, ωwheel):
         # J: Inertia (kgm^2)
         # k: Spring constant (N/m)
         # b: Damping coefficient (and Viscous Friction) (Ns/m)
         # N: Number of teeth
         # r: Radius (m)
-        J1 = 0.02521  # Motor
-        J2a = 0.0001141295  # Motor Cover shaft
-        J2b = 0.0000632043  # Motor Internal shaft
-        J2 = J2a + J2b  # Motor Shaft (Currently not including bolts, washers, circlip, spacer, cotter pin, castle nut, or bearing)
-        J3 = 0.0000228259  # Motor Sprocket
-        J4 = 0  # Chain
-        J5a = 0.0019226425  # Diff Sprocket
-        J5b = 0.000155099  # Sprocket Adapter
-        J5 = J5a + J5b  # Diff Sprocket (Currently not including bolts, washers,
-        J6 = 0  # Differential
-        J7 = 0.0000556015  # Right Half-shaft
-        J8 = 0  # Left Half-shaft
-        J9 = J1  # Wheel
+        self.J1 = 0.02521  # Motor
+        self.J2a = 0.0001141295  # Motor Cover shaft
+        self.J2b = 0.0000632043  # Motor Internal shaft
+        self.J2 = self.J2a + self.J2b  # Motor Shaft (Currently not including bolts, washers, circlip, spacer, cotter pin, castle nut, or bearing)
+        self.J3 = 0.0000228259  # Motor Sprocket
+        self.J4 = 0  # Chain
+        self.J5a = 0.0019226425  # Diff Sprocket
+        self.J5b = 0.000155099  # Sprocket Adapter
+        self.J5 = self.J5a + self.J5b  # Diff Sprocket (Currently not including bolts, washers,
+        self.J6 = 0  # Differential
+        self.J7 = 0.0000556015  # Right Half-shaft
+        self.J8 = 0  # Left Half-shaft
+        self.J9 = self.J1  # Wheel
 
-        k1 = 0  # Chain
+        self.k1 = 0  # Chain
 
-        b1 = 0  # Motor
-        b2 = 0  # Chain
-        b3 = 0  # Differential
-        b4 = 0  # Tripod
+        self.b1 = 0  # Motor
+        self.b2 = 0  # Chain
+        self.b3 = 0  # Differential
+        self.b4 = 0  # Tripod
 
-        N1 = 42  # Motor sprocket
-        N2 = 12  # Diff sprocket
+        self.N1 = 42  # Motor sprocket
+        self.N2 = 12  # Diff sprocket
 
-        r1 = 0.05306809079 / 2  # Motor sprocket
-        r2 = 0.1941806998 / 2  # Diff sprocket
-        r3 = 0.2032  # Wheel
+        self.r1 = 0.05306809079 / 2  # Motor sprocket
+        self.r2 = 0.1941806998 / 2  # Diff sprocket
+        self.r3 = 0.2032  #
 
-    def transfer_function(self, Tmotor, ωwheel):
+        self.T1 = Tmotor
+        self.ω4 = ωwheel
+
+    def transfer_function(self):
         # T: Torque (Nm)
         # θ: Angular position (rad)
         # ω: Angular velocity (rad/s)
@@ -46,14 +45,15 @@ class Drivetrain:
         # x: Linear position (m)
         # v: Linear velocity (m/s)
         # a: Linear acceleration (m/s^2)
-        T1 = Tmotor  # Motor input
-        ω4 = ωwheel  # Whee speed input
-        F1 = (T1 * r2 * (J6 + J7 + J9)) / (r2 * r1 * (J1 + J2) + r2 * r2 * (J6 + J7 + J9))
-        α1 = (T1 - F1 * r1) / (J1 + J2)
-        α4 = α1 * r2 / r1
-        return α4
+        F1 = (self.T1 * self.r2 * (self.J6 + self.J7 + self.J9)) / (self.r2 * self.r1 * (self.J1 + self.J2) + self.r2 * self.r2 * (self.J6 + self.J7 + self.J9))
+        α1 = (self.T1 - F1 * self.r1) / (self.J1 + self.J2)
+        α4 = α1 * self.r2 / self.r1
+        T4 = α4 * (self.J6 + self.J7 + self.J9 + (self.N2 / self.N1) ** 2 * (self.J1 + self.J2))
+        print(T4)
+        return T4
 
 Tin = 230
 w = 0
-print(Drivetrain.transfer_function(0,Tin,w) * r3)
-print(Tin * N2 / N1 / r3)
+
+Drivetrain(Tin,w).transfer_function()
+print(Tin * 42 / 12)
