4e5372afd8c1e90505f2cfebe4287270bf006964
erichbahm <97556713+erichbahm@users.noreply.github.com>
2024-04-24 12:34:12 -0400

    Added method to calculate power output of battery pack

--- src/high_voltage_module.py
+++ src/high_voltage_module.py
@@ -3,12 +3,14 @@ ASSUME dI/dt = 0
 COEFFICIENTS
  R: Resistance (Ohm)
  L: Inductance (H)
- Kb: Induced Voltage Constant (VRMS/RPM)
+ Kb: Induced Voltage Constant (VRMS*s/rad)
 VARIABLES
  V: Voltage (V)
  I: Current (A)
  ω: Angular velocity (rad/s)
 """
+from math import pi
+
 class HighVoltage:
     def __init__(self, VBat, Ibat, ωmotor):
         self.R1 = 3 * 0.01548 # Motor Internal resistance
@@ -23,7 +25,9 @@ class HighVoltage:
 
     def tf(self):
         s = 0
-        I1 = (self.V1 - self.Kb * self.ω1) / (s * self.L1 + self.R1)
+        V1 = self.V1 - self.Kb * self.ω1 * (2 * pi) / 60
+        I1 = (self.V1 - self.Kb * self.ω1 * (2 * pi) / 60) / (s * self.L1 + self.R1)
+        P1 = V1 * I1
         if I1 > self.Ibat: I1 = self.Ibat
         if I1 < 0: I1 = 0
         return I1
