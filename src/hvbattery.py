a2505eb93cf9456ba1fd70052cf671c454aa17ec
Erich <bahmerich@gmail.com>
2024-08-04 04:33:50 -0400

    Reduced battery model to 3 important functions

--- src/hvbattery.py
+++ src/hvbattery.py
@@ -1,56 +1,29 @@
-import numpy as np
+def calc_peak_power(car, Voc): # FUTURE: account for internal resistance thermal sensitivity
+    If = car.attrs['max_current']
+    series = car.attrs['cells_series']
+    parallel = car.attrs['cells_parallel']
+    R = car.attrs['cell_resistance']
+    Vmin = 2.5
+
+    Im = min(If / parallel, (Voc / series - Vmin) / R)
+    return series * parallel * Im * (Voc / series - R * Im)
 
 
-class Cell():
-    def __init__(self):
-        self.vmax = 4.2  # Max voltage, V
-        self.vmin = 2.5  # Min voltage, V
-        self.dcr = 0.02 # DC resistance, Ohm
-        self.c = 10.3 # Capacity, Wh
-        self.e = self.c * 3600  # Energy, J
-        self.m = 0.046 # Mass, kg
-        self.cp = 900  # Special heat capacity, J/kgK
+def calc_heat_gen(car, Voc, P):
+    R = car.attrs['cell_resistance']
+    series = car.attrs['cells_series']
+    parallel = car.attrs['cells_parallel']
+    P /= series * parallel
 
-    def soev(self, v):
-        return (v - self.vmin) / (self.vmax - self.vmin)
+    I = Voc / (2 * R) - (Voc**2 / (4 * R**2) - P / R)**0.5
+    return (I**2 * R) * series * parallel
 
-    def soee(self, e):
-        return e / self.e
 
-    def voc(self, e):
-        return self.vmin + e / self.e * (self.vmax - self.vmin)
-
-    def pwr_iter(self, na0, p, dt):
-        na1 = np.zeros_like(na0)
-        na1[0] = na0[0] + dt
-        na1[8] = na0[8] - na0[7]
-        na1[2] = self.voc(na1[8])
-        na1[3] = self.dcr
-        na1[6] = self.vmin * (na1[2] - self.vmin) / na1[3]
-        na1[1] = min(p,na1[6])
-        na1[4] = na1[2] / 2 / na1[3] - (na1[2] ** 2 / (4 * na1[3] ** 2) - p / na1[3]) ** 0.5
-        if na1[4] != 0:
-            na1[5] = p / na1[4]
-        else:
-            na1[5] = na1[2]
-        # skip 6
-        na1[7] = p * dt
-        # skip 8
-        na1[9] = self.soee(na1[8])
-        na1[10] = na1[4] ** 2 * na1[3]
-        na1[11] = na0[11] + na0[10] * dt
-        na1[12] = na1[10] / (self.m * self.cp) * dt
-        na1[13] = na0[13] + na0[12]
-        return [na1]
-
-    def pwr_cycle(self, p, T0, v0, t1, dt=0.0004):
-        t = 0
-        na = np.array([[0, 0, v0, self.dcr, 0, v0, self.vmin * (v0 - self.vmin) / self.dcr, 0, self.soev(v0) * self.e,
-                        self.soev(v0), 0, 0, 0, T0]])
-        # t, P, Voc, dcr, I, Vcc, PPk, dE, E, SOE, Qgen, Q, dT, T
-        for i in range(p.size):
-            t = 0
-            while t < t1[i] + dt:
-                na = np.append(na, self.pwr_iter(na[np.shape(na)[0]-1], p[i], dt),axis=0)
-                t += dt
-        return na
+def calc_Voc(car, E):
+    series = car.attrs['cells_series']
+    parallel = car.attrs['cells_parallel']
+    capacity = car.attrs['cell_capacity']
+    Em = series * parallel * capacity * 3600
+    Vmax = 4.5
+    Vmin = 2.5
+    return (Vmin + E / Em * (Vmax - Vmin)) * series
