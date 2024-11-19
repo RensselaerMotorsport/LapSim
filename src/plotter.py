4235fe26bec6809adef46f0a031c99774dcc181f
Noah Bennett <142277578+nbennett7z@users.noreply.github.com>
2024-11-19 17:00:36 -0500

    Update plotter.py
    Update plotter to take optimized ratio as a parameter for plotting

--- src/plotter.py
+++ src/plotter.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 
 
-def plot_single_yaxis(x, y, x_axis, y_axis, y_labels, y_colors, y_ls, y_lim=None, save_plot=None):
+def plot_single_yaxis(x, y, x_axis, y_axis, y_labels, y_colors, y_ls, y_lim=None, save_plot=None, vline=None, vline_style=None):
+    import matplotlib.pyplot as plt
+
     fig, ax1 = plt.subplots()
     fig.set_figwidth(6.4 * 2)
     fig.set_figheight(4.8 * 2)
@@ -9,18 +11,25 @@ def plot_single_yaxis(x, y, x_axis, y_axis, y_labels, y_colors, y_ls, y_lim=None
     ax1.set_ylabel(y_axis)
     ax1.tick_params(axis='x', labelcolor='black')
     ax1.tick_params(axis='y', labelcolor='black')
+
     for i in range(len(y_labels)):
         ax1.plot(x, y[i], label=y_labels[i], color=y_colors[i], ls=y_ls[i])
-        ax1.set_xlim(min(x), max(x))
+    ax1.set_xlim(min(x), max(x))
+
     if y_lim:
         ax1.set_ylim(y_lim)
     else:
         ax1.set_ylim(0, )
+
+    if vline is not None:
+        ax1.axvline(x=vline, **(vline_style or {'color': 'black', 'linestyle': '--'}))
+
     plt.title('')
     plt.suptitle('')
     fig.legend(loc='upper center', ncols=len(y_labels))
     fig.tight_layout()
     plt.grid()
+
     if save_plot:
         plt.savefig(save_plot)
     plt.show()
